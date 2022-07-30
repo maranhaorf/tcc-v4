964c0f135ae95b21c43697d8943043a4948f2e0c
maranhaorf <74361153+maranhaorf@users.noreply.github.com>
2022-07-30 10:35:29 -0300

    comit index

---
+++ public/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('Material Dashboard')])
+
+@section('content')
+<div class="container" style="height: auto;">
+    <div class="row justify-content-center">
+        <div class="col-lg-7 col-md-8">
+
+        </div>
+    </div>
+</div>
+@endsection
